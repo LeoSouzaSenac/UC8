eb4223a14d3f88ed636a8821d13256654e8acd26
LeoSouzaSenac <152904287+LeoSouzaSenac@users.noreply.github.com>
2025-02-26 21:01:00 -0300

    Add files via upload

---
+++ Pokemon/Pokemon.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pokemon;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Pokemon {
+    String nome;
+    String tipo;
+    int nivel;
+    
+    public Pokemon(String nome, String tipo, int nivel) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.nivel = nivel;
+    }
+}
